6ca1bbed75f91699120c0a7856217a3ede570ee2
Lake Brant <lake.brant@du.edu>
2021-01-04 12:06:46 -0700

    uploaded data into html

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Get a reference to the table body
+var tbody = d3.select('tbody');
+
+// Console.log the UFO data from data.js
+console.log(tableData);
+
+// Load data
+tableData.forEach((ufoReport) => {
+    var row = tbody.append('tr');
+    Object.entries(ufoReport).forEach(([key, value]) => {
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
