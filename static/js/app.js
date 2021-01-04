1b9dc18845cccc657deea678873237de58e6c5a3
Lake Brant <lake.brant@du.edu>
2021-01-04 13:23:56 -0700

    created filter to change table by date

--- static/js/app.js
+++ static/js/app.js
@@ -7,7 +7,7 @@ var tableData = data;
 var tbody = d3.select('tbody');
 
 // Console.log the UFO data from data.js
-console.log(tableData);
+// console.log(tableData);
 
 // Load data
 tableData.forEach((ufoReport) => {
@@ -17,3 +17,50 @@ tableData.forEach((ufoReport) => {
         cell.text(value);
     });
 });
+
+// Create filter for the tableData
+
+// Select the button
+var button = d3.select('#filter-btn');
+
+// Select the form
+var form = d3.select('#form');
+
+// Create the event handlers
+button.on('click', runEnter);
+form.on('submit', runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+    
+
+    // Prevent page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select('#datetime');
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property('value');
+
+    console.log(inputValue);
+    // console.log(tableData);
+
+    var filteredData = tableData.filter(data => data.datetime === inputValue);
+
+    console.log(filteredData);
+
+    // Clear the existing data to be replaced by the new data 
+    tbody.html('');
+
+    // Add in the filtered data
+    filteredData.forEach((filteredReport) => {
+        var row = tbody.append('tr');
+        Object.entries(filteredReport).forEach(([key, value]) => {
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    });
+
+
+};
